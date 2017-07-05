ca356cacaa3d93319ccc02f197994ca301c554ea
Green <854232507@qq.com>
2017-07-05 14:45:31 +0800

    finish chapter 4

--- jvmgo/main.go
+++ jvmgo/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"jvmgo/ch03/classfile"
-	"jvmgo/ch03/classpath"
-	"strings"
+	"jvmgo/jvmgo/rtda"
 )
 
 func main() {
@@ -19,38 +17,41 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
-	className := strings.Replace(cmd.class, ".", "/", -1)
-	cf := loadClass(className, cp)
-	fmt.Println(cmd.class)
-	printClassInfo(cf)
+	frame := rtda.NewFrame(100, 100)
+	testLocalVars(frame.LocalVars())
+	testOperandStack(frame.OperandStack())
 }
 
-func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
-	classData, _, err := cp.ReadClass(className)
-	if err != nil {
-		panic(err)
-	}
-	cf, err := classfile.Parse(classData)
-	if err != nil {
-		panic(err)
-	}
-	return cf
+func testLocalVars(vars rtda.LocalVars) {
+	vars.SetInt(0, 100)
+	vars.SetInt(1, -100)
+	vars.SetLong(2, 299794580)
+	vars.SetLong(4, -299794580)
+	vars.SetFloat(6, 3.1415926)
+	vars.SetDouble(7, 2.7182818245)
+	vars.SetRef(9, nil)
+	println(vars.GetInt(0))
+	println(vars.GetInt(1))
+	println(vars.GetLong(2))
+	println(vars.GetLong(4))
+	println(vars.GetFloat(6))
+	println(vars.GetDouble(7))
+	println(vars.GetRef(9))
 }
 
-func printClassInfo(cf *classfile.ClassFile) {
-	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
-	fmt.Printf("contains count: %v\n", len(cf.ConstantPool()))
-	fmt.Printf("access flags: 0x%x\n", cf.AccessFlags())
-	fmt.Printf("this class: %v\n", cf.ClassName())
-	fmt.Printf("super class: %v\n", cf.SuperClassName())
-	fmt.Printf("interfaces: %v\n", cf.InterfaceNames())
-	fmt.Printf("fields count: %v\n", len(cf.Fields()))
-	for _, f := range cf.Fields() {
-		fmt.Printf(" %s\n", f.Name())
-	}
-	fmt.Printf("methods count: %v\n", len(cf.Methods()))
-	for _, m := range cf.Methods() {
-		fmt.Printf(" %s\n", m.Name())
-	}
+func testOperandStack(ops *rtda.OperandStack) {
+	ops.PushInt(100)
+	ops.PushInt(-100)
+	ops.PushLong(299794580)
+	ops.PushLong(-299794580)
+	ops.PushFloat(3.1415926)
+	ops.PushDouble(2.7182818245)
+	ops.PushRef(nil)
+	println(ops.PopRef())
+	println(ops.PopDouble())
+	println(ops.PopFloat())
+	println(ops.PopLong())
+	println(ops.PopLong())
+	println(ops.PopInt())
+	println(ops.PopInt())
 }
---
+++ jvmgo/rtda/slot.go
@@ -0,0 +1,8 @@
+package rtda
+
+// Slot is the element that stored in LocalVars
+
+type Slot struct {
+	num int32
+	ref *Object
+}
