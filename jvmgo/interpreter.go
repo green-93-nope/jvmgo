abc59fbe714cf39d4e664eca01daeede5a1bcf21
Green <854232507@qq.com>
2017-08-07 08:43:28 +0800

    better display of debug info

--- jvmgo/interpreter.go
+++ jvmgo/interpreter.go
@@ -51,8 +51,9 @@ func logFrames(thread *rtda.Thread) {
 		frame := thread.PopFrame()
 		method := frame.Method()
 		className := method.Class().Name()
-		fmt.Printf(">> pc: %4d %v.%v%v",
-			frame.NextPC(), className, method.Name(), method.Descriptor())
+		lineNum := method.GetLineNumber(frame.NextPC())
+		fmt.Printf(">> line:%4d pc: %4d %v.%v%v \n",
+			lineNum, frame.NextPC(), className, method.Name(), method.Descriptor())
 	}
 }
 
